54902b79c8f6a97a4458bf6b287537bfeb849219
Arina Miahkykh <79450238+Arina-M@users.noreply.github.com>
2023-06-27 12:03:36 +0300

    styled task

---
+++ src/components/section/Section.styled.jsx
@@ -0,0 +1,19 @@
+import styled from 'styled-components';
+
+export const Container = styled.section`
+  padding: 20px;
+  background-color: #fff;
+  border-radius: 20px;
+  overflow: hidden;
+  box-shadow: 10px 10px 10px rgba(46, 47, 66, 0.08),
+    0px 1px 1px rgba(46, 47, 66, 0.16), 2px 2px 1px rgba(46, 47, 66, 0.08);
+  background-color: #edf3fb;   
+`;
+
+export const SectionTitle = styled.h1`
+  text-align: center;
+  margin-bottom: 20px;
+  font-size: 48px;
+  font-weight: 700;
+  color: black;
+`;
